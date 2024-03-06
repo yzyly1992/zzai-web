4222865ea97774c3eaa04b65c7d8c8b1e8f473dd
David Yang <zyang03@risd.edu>
2024-03-06 13:27:49 -0800

    add sitemap.xml and error page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
         openShare = !openShare;
     }
     function copyPath() {
-        navigator.clipboard.writeText($page.url.pathname);
+        navigator.clipboard.writeText(`https://zzai.tech${$page.url.pathname}`);
         showMessage = true;
         setTimeout(() => {
             showMessage = false;
@@ -15,7 +15,7 @@
     }
     function sendEmail() {
         const subject = 'Check out this page';
-        const body = `I found this page and thought you might like it. ${$page.url.pathname}`;
+        const body = `I found this page and thought you might like it. https://zzai.tech${$page.url.pathname}`;
         window.open(`mailto:?subject=${subject}&body=${body}`);
         openShare = false;
     }
@@ -44,21 +44,21 @@
                     <p>Send as Email</p>
                 </button>
                 <hr>
-                <a href="https://www.facebook.com/sharer/sharer.php?u={$page.url.pathname}" target="_blank" rel="noopener noreferrer" aria-label="Share on Facebook">
+                <a href="https://www.facebook.com/sharer/sharer.php?u=https://zzai.tech{$page.url.pathname}" target="_blank" rel="noopener noreferrer" aria-label="Share on Facebook">
                     <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
                     </svg>
                     <p>Share to Facebook</p>
                 </a>
                 <!-- share to linkedin -->
-                <a href="https://www.linkedin.com/sharing/share-offsite/?url={$page.url.pathname}" target="_blank" rel="noopener noreferrer" aria-label="Share on LinkedIn">
+                <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://zzai.tech{$page.url.pathname}" target="_blank" rel="noopener noreferrer" aria-label="Share on LinkedIn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
                     </svg>
                     <p>Share to LinkedIn</p>
                 </a>
                 <!-- share to twitter-x -->
-                <a href="https://twitter.com/intent/tweet?url={$page.url.pathname}" target="_blank" rel="noopener noreferrer" aria-label="Share on Twitter">
+                <a href="https://twitter.com/intent/tweet?url=https://zzai.tech{$page.url.pathname}" target="_blank" rel="noopener noreferrer" aria-label="Share on Twitter">
                     <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
                         <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
                     </svg>
---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,41 @@
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function GET({ platform }) {
+    let result: any = await platform?.env.DB.prepare(
+      "SELECT slug FROM posts"
+    ).run();
+
+    return new Response(
+      `
+      <?xml version="1.0" encoding="UTF-8" ?>
+      <urlset
+        xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="https://www.w3.org/1999/xhtml"
+        xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+        xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+      >
+  
+         <url>
+          <loc>https://zzai.tech</loc>
+          <changefreq>daily</changefreq>
+          <priority>0.7</priority>
+        </url>
+        ${result.results.map((post: any) => {
+            return `
+            <url>
+                <loc>https://zzai.tech/blog/${post.slug}</loc>
+                <changefreq>daily</changefreq>
+                <priority>0.7</priority>
+            </url>
+            `;
+        }).join("")}
+  
+      </urlset>`.trim(),
+      {
+        headers: {
+          "Content-Type": "application/xml",
+        },
+      }
+    );
+  }
