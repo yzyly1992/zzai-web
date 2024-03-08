2455425ae25fb1c3bf325d9cf3da921a8673b89e
David Yang <zyang03@risd.edu>
2024-03-07 22:35:25 -0800

    add ads.txt

---
+++ src/routes/ads.txt/+server.ts
@@ -0,0 +1,7 @@
+export async function GET() {
+    return new Response('google.com, pub-2669927207063183, DIRECT, f08c47fec0942fa0', {
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    });
+}
