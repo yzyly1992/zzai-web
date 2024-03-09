6c840ea3fb53435f8cb2a1ca893e520bb34e6bc8
David Yang <zyang03@risd.edu>
2024-03-08 22:25:07 -0800

    update query order by dec id

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from "@sveltejs/kit";
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET({ platform }) {
   let result = await platform?.env.DB.prepare(
-    "SELECT slug,title,cover,subtitle FROM posts LIMIT 5"
+    "SELECT slug,title,cover,subtitle FROM posts ORDER BY id DESC LIMIT 5"
   ).run();
   return new Response(JSON.stringify(result));
 }
